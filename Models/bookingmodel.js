96630b93ad486d4e9a27a629cab50e2d8f7b888e
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-09-16 19:58:11 +1000

    landing page and search ui implementations with modified dbconnection

--- Models/bookingmodel.js
+++ Models/bookingmodel.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-//Note : I removed my credentials since I didn't use an .env file to store the data.
-const mongoURI = 'mongodb+srv://keerthuvije:ZEZS1JlZIvPPwkPe@cluster0.avmbigb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Replace with your MongoDB connection string
 
 const bookingSchema = new mongoose.Schema({
     name: String,
@@ -11,18 +9,6 @@ const bookingSchema = new mongoose.Schema({
 
 const Booking = mongoose.model('Booking', bookingSchema);
 
-const runDBConnection = async () => {
-    try {
-        await mongoose.connect(mongoURI);
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        throw error;
-    }
-};
-
 module.exports = {
-    Booking,
-    runDBConnection,
-    mongoURI
+    Booking
 };
