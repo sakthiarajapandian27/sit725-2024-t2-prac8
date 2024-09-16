9b8ef1947584e2c87ec5753e2d955ccceb6e1db9
Sandali Samarawickrama <wathmali123@gmail.com>
2024-09-16 11:53:57 +1000

    update model with ownerId and sitterId

--- Models/bookings.js
+++ Models/bookings.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   bookingId: String,
-  userId: String,
-  userType: String,
+  ownerId: String,
+  sitterId: String,
   ownerName: String,
   sitterName: String,
   date: Date,
   address: String,
   confirmation: Boolean,
+  confirmed: Boolean,
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
 
 // Find records by user id
 const findByUserId = (userId) => {
-  return Booking.find({ userId: userId });
+  return Booking.find({
+    $or: [{ ownerId: userId }, { sitterId: userId }],
+  });
 };
 
 const findByIdAndUpdate = (id, confirmation) => {
