7a0c310d5c2bae26cd34c890b1e32eed441e1b74
Sandali Samarawickrama <wathmali123@gmail.com>
2024-09-25 13:31:48 +1000

    modify server.js with booking changes

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const path = require("path");
 const bookingController = require("./Controllers/bookingcontroller");
 const webRoutes = require("./Routes/webroutes"); // Import static file routes
 const apiRoutes = require("./Routes/apiroutes"); // Import API routes
-const http = require("http");
-const socketIo = require("socket.io");
+const bookingRoutes = require("./Routes/bookings");
 
 const app = express();
-const server = http.createServer(app); // Create an HTTP server
-const io = socketIo(server); // Attach socket.io to the server
 const port = 3040;
+
+const socket = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = socket(server);
 
 // Middleware
 app.use(bodyParser.json());
@@ -25,17 +27,20 @@ app.use(express.static(path.join(__dirname, "Views")));
 // Use the routes
 app.use("/", webRoutes); // Serve static file routes
 app.use("/api", apiRoutes); // Serve API routes
+app.use("/api2", bookingRoutes);
 
-// Socket.io setup
+// Socket setup
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("user connected");
 
-  // Example: Emit a message to the connected client
-  socket.emit("message", "Welcome to the PawFinders Service Booking!");
-
-  // Handle disconnection
   socket.on("disconnect", () => {
     console.log("User disconnected");
+  });
+
+  socket.on("Transaction", (data) => {
+    socket.broadcast.emit("Transaction", {
+      message: "You have a new message",
+    });
   });
 });
 
