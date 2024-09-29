29a55f8d7082785626fe2e4f2a70ced5228a9e57
Sandali Samarawickrama <wathmali123@gmail.com>
2024-09-30 09:49:36 +1000

    removed login check from walker profile

--- public/js/walkerProfile.js
+++ public/js/walkerProfile.js
@@ -7,15 +7,13 @@ $(document).ready(function () {
 
   const location = JSON.parse(localStorage.getItem("location"));
 
-  if (profileData) {
-    $("#firstName").text(sitterData.firstName);
-    $("#lastName").text(sitterData.lastName);
-    $("#phone").text(sitterData.phone);
-    $("#email").text(sitterData.email);
-    $("#address").text(sitterData.address);
-    $("#suburb").text(sitterData.suburb);
-    $("#postalCode").text(sitterData.postalCode);
-  }
+  $("#firstName").text(sitterData.firstName);
+  $("#lastName").text(sitterData.lastName);
+  $("#phone").text(sitterData.phone);
+  $("#email").text(sitterData.email);
+  $("#address").text(sitterData.address);
+  $("#suburb").text(sitterData.suburb);
+  $("#postalCode").text(sitterData.postalCode);
 
   // Click event for Leave a Review button
   $("#review").click(function () {
