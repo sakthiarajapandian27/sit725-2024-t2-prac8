6ad2efd5bc8715d8a3fee343420486a9b45f911c
sakthiarajapandian <sakthiapandian27@gmail.com>
2024-09-30 12:48:59 +1000

    Fix of missed login functionality,sign up web routes and profile integration.

--- services/login.service.js
+++ services/login.service.js
@@ -1,7 +1,10 @@
 const { Registration} = require('../Models/signupModel');
+const {Walker} = require('../Models/signupModelWalker');
 
-exports.findUser = (email,password)=>{
-    return  Registration.findOne({"email": email,"password": password});
-    
-
+exports.findUser = (email, password, type) => {
+    if (type === 'OWNER') {
+        return Registration.findOne({ "email": email, "password": password });
+    } else if (type === 'WALKER') {
+        return Walker.findOne({ "email": email, "password": password });
+    } 
 }
